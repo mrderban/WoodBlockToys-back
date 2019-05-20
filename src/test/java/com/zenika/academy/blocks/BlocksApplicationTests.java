1a0cb3f15becf45835245d17b56ceb12442878de
‘mrderban’ <mrderban@gmail.com>
2019-05-20 10:59:19 +0200

    initial commit

---
+++ src/test/java/com/zenika/academy/blocks/BlocksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zenika.academy.blocks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlocksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
