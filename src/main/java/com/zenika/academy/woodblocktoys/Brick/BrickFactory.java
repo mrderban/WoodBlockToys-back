4d5a824a20f46690cd0a9d801eba18f3d1e38f54
‘mrderban’ <mrderban@gmail.com>
2019-05-26 22:36:34 +0200

    test validation knapsack algorithm naive solution w/ brick test entity

--- src/main/java/com/zenika/academy/woodblocktoys/Brick/BrickFactory.java
+++ src/main/java/com/zenika/academy/woodblocktoys/Brick/BrickFactory.java
@@ -9,49 +9,47 @@ public class BrickFactory {
     //function that maximizes the number of bricks picked for a given price value threshold
     public static Solution knapSack(List<Brick> inputBrickList, Solution currentSolution, int pos) {
 
-        Brick currentBrick = inputBrickList.get(pos);
-
         //****************END OF EXPLORATION CASES****************//
         //end of list & out of capacity => return  currentSolution, no need to go further
-        if (currentSolution.getCapacity() == 0 || pos == 0) {
+        if (currentSolution.getCapacity() == 0 || pos < 0) {
             return currentSolution;
         }
 
+        Brick currentBrick = inputBrickList.get(pos);
 
         //if over-priced, just move to the pos to the left cause we can't keep this brick,
-        else if (currentBrick.getPrice() > currentSolution.getCapacity()) {
+        if (currentBrick.getPrice() > currentSolution.getCapacity()) {
             return knapSack(inputBrickList, currentSolution, pos - 1);
         }
 
         //else switch on or off the brick at position pos => we create temporary solutions and compare them
         //switch ON current brick at position pos
-        Solution tempSol1 = Solution.builder()
+        Solution tempRightSol = Solution.builder()
                 .brickList(new ArrayList<>())
                 .capacity(currentSolution.getCapacity() - currentBrick.getPrice())
                 .value(currentSolution.getValue() + 1)
                 .build();
         //copy currentSolution brickList content
-        tempSol1.getBrickList().addAll(currentSolution.getBrickList());
-        //add current brick to tempSol1 brickList
-        tempSol1.getBrickList().add(currentBrick);
-
+        tempRightSol.getBrickList().addAll(currentSolution.getBrickList());
+        //add current brick to tempRightSol brickList
+        tempRightSol.getBrickList().add(currentBrick);
 
         //switch OFF current brick at position pos (exclude it from original bricklist => no added value, no decreased capacity (no added weight)
-        Solution tempSol2 = Solution.builder()
+        Solution tempLeftSol = Solution.builder()
                 .brickList(new ArrayList<>())
                 .capacity(currentSolution.getCapacity())
                 .value(currentSolution.getValue())
                 .build();
         //copy currentSolution brickList content
-        tempSol1.getBrickList().addAll(currentSolution.getBrickList());
-        //do not add current brick to tempSol2 brickList
+        tempLeftSol.getBrickList().addAll(currentSolution.getBrickList());
+        //do not add current brick to tempLeftSol brickList
 
         //recursive calls
-        Solution tempResult1 = knapSack(inputBrickList, tempSol1, pos - 1);
-        Solution tempResult2 = knapSack(inputBrickList, tempSol2, pos - 1);
+        Solution tempRightResult = knapSack(inputBrickList, tempRightSol, pos - 1);
+        Solution tempLeftResult = knapSack(inputBrickList, tempLeftSol, pos - 1);
 
         //return the brickList of the solution with max value
-        return tempResult1.getValue() > tempResult2.getValue() ? tempResult1 : tempResult2;
+        return tempRightResult.getValue() > tempLeftResult.getValue() ? tempRightResult : tempLeftResult;
 
     }
 }
--- src/test/java/com/zenika/academy/woodblocktoys/Brick/BrickFactoryTest.java
+++ src/test/java/com/zenika/academy/woodblocktoys/Brick/BrickFactoryTest.java
@@ -3,7 +3,9 @@ package com.zenika.academy.woodblocktoys.Brick;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
@@ -11,7 +13,7 @@ public class BrickFactoryTest {
 
     @Test
     public void knapSack() {
-
+        //input
         Brick brick1 = Brick.builder()
                 .price(0.5)
                 .id(1L)
@@ -20,6 +22,59 @@ public class BrickFactoryTest {
                 .price(0.5)
                 .id(2L)
                 .build();
+
+        Brick brick3 = Brick.builder()
+                .price(1)
+                .id(3L)
+                .build();
+
+
+        List<Brick> inputBrickList = new ArrayList<>();
+        inputBrickList.add(brick1);
+        inputBrickList.add(brick2);
+        inputBrickList.add(brick3);
+
+
+        Solution inputSolution = Solution.builder()
+                .value(0)
+                .capacity(1)
+                .brickList(new ArrayList<>())
+                .build();
+
+        //output expected
+        List<Brick> brickList1 = new ArrayList<>();
+        brickList1.add(brick1);
+        brickList1.add(brick2);
+
+
+        Solution expectedSolution = Solution.builder()
+                .value(1)
+                .capacity(0)
+                .brickList(brickList1)
+                .build();
+
+        Solution resultSolution = BrickFactory.knapSack(inputBrickList, inputSolution, inputBrickList.size() - 1);
+        Set<Brick> expectedBrickSet = new HashSet<Brick>(expectedSolution.getBrickList());
+        Set<Brick> resultBrickSet = new HashSet<Brick>(resultSolution.getBrickList());
+
+
+        assertThat(expectedBrickSet).isEqualTo(resultBrickSet);
+        assertThat(expectedSolution.getCapacity()).isEqualTo(expectedSolution.getCapacity());
+        assertThat(expectedSolution.getValue()).isEqualTo(expectedSolution.getValue());
+    }
+
+    @Test
+    public void knapSackTest2() {
+        //input
+        Brick brick1 = Brick.builder()
+                .price(0.5)
+                .id(1L)
+                .build();
+        Brick brick2 = Brick.builder()
+                .price(0.5)
+                .id(2L)
+                .build();
+
         Brick brick3 = Brick.builder()
                 .price(1)
                 .id(3L)
@@ -29,30 +84,38 @@ public class BrickFactoryTest {
                 .id(4L)
                 .build();
 
-        List<Brick> brickList = new ArrayList<>();
-        brickList.add(brick1);
-        brickList.add(brick2);
-        brickList.add(brick3);
-        brickList.add(brick4);
+        List<Brick> inputBrickList = new ArrayList<>();
+        inputBrickList.add(brick1);
+        inputBrickList.add(brick2);
+        inputBrickList.add(brick3);
+        inputBrickList.add(brick4);
 
-        Solution solutionIn = Solution.builder()
+        Solution inputSolution = Solution.builder()
                 .value(0)
                 .capacity(2)
                 .brickList(new ArrayList<>())
                 .build();
 
+        //output expected
         List<Brick> brickList1 = new ArrayList<>();
         brickList1.add(brick1);
         brickList1.add(brick2);
         brickList1.add(brick3);
 
 
-        Solution solutionOut = Solution.builder()
-                .value(3)
+        Solution expectedSolution = Solution.builder()
+                .value(2)
                 .capacity(0)
                 .brickList(brickList1)
                 .build();
 
-        assertThat(BrickFactory.knapSack(brickList, solutionIn, 3)).isEqualTo(solutionOut);
+        Solution resultSolution = BrickFactory.knapSack(inputBrickList, inputSolution, inputBrickList.size() - 1);
+        Set<Brick> expectedBrickSet = new HashSet<Brick>(expectedSolution.getBrickList());
+        Set<Brick> resultBrickSet = new HashSet<Brick>(resultSolution.getBrickList());
+
+
+        assertThat(expectedBrickSet).isEqualTo(resultBrickSet);
+        assertThat(expectedSolution.getCapacity()).isEqualTo(expectedSolution.getCapacity());
+        assertThat(expectedSolution.getValue()).isEqualTo(expectedSolution.getValue());
     }
 }
